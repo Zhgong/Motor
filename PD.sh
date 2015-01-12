8609a33ca9265cb20a1ab94db64b55d9939d8a01
gongzhou <sean.gongz@gmail.com>
2015-01-13 00:05:13 +0100

    run PD run Parallels

---
+++ PD.sh
@@ -0,0 +1,41 @@
+# 1. change date to the past
+# 2. run parallels
+# 3. change date back
+
+
+
+# old date
+
+o_month=01
+o_day=10
+o_year=15
+
+# get currentdate
+month=`date +'%m'`
+day=`date +"%d"`
+hour=`date +"%H"`
+minute=`date +"%M"`
+year=`date +"%y"`
+
+curr_dty=$month$day$hour$minute$year
+old_dty=$o_month$o_day$hour$minute$o_year
+
+echo "current date is:"
+date
+
+# change date
+echo "changing date to old one"
+sudo date $old_dty
+
+# start PD
+open /Applications/Parallels\ Desktop.app/
+
+# wait for 5 seconds
+sleep 5
+
+# change it back
+echo "changing date back"
+sudo date $curr_dty
+
+# synct date back
+sudo ntpdate -u time.apple.com
